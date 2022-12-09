4b429ab4b746a1a1549098600ec0d3151ba34b75
Artemii100500 <Artemii100500@gmail.com>
2022-12-09 21:35:13 +0700

    123

---
+++ Lesson912/Lesson912/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson912
+{
+     public class Class1
+    {
+        static void Main()
+        {
+            int a = 1 + 4;
+            int b = 2 - 4;
+            int c = 3 * 4;
+            int d = 10%4;
+            float f = 6 / 4;
+            Console.WriteLine("{0} {1} {2} {3} {4}",a,b,c,d,f);
+        }
+    }
+}
